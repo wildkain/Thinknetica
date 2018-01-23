92f867c00ff60928dd1d40837f81f75c56e59151
Stanislav <wildkain@gmail.com>
2018-01-23 23:59:17 +0300

    Almost done

---
+++ Lesson1/Ideal_weight.rb
@@ -0,0 +1,26 @@
+# This little program will help you know your ideal weight (but it's not totally true)
+# Let's start
+
+
+programm_name = "'Ideal weight'"
+
+
+p "Hi, i am a smarty programm, call me #{programm_name}"
+p "I can help you find out your ideal weight"
+p "Let's get acquainted! What is your name?"
+
+#take user input and save to variable
+name = gets.chomp
+
+p "Fine, #{name}. Now, I need to know your height in centimeters"
+
+#take user's input and save to variable, convert to int
+height = gets.chomp.to_i 
+
+p "Exelent! So,  I need a little time to calculate ..."
+
+# Take 'height' parameter and calculate "ideal_weight" as 'height -110'
+ideal_weight = height - 110
+sleep(2)
+p ideal_weight
+

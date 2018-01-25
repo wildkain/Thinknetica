1e8d2040ecb9f4ddacbfe7baf930ab079f035699
Stanislav <wildkain@gmail.com>
2018-01-26 00:31:38 +0300

    Фикс замечаний по логикеи стилистике, смена логики на использование массива

--- Lesson1/Pifagor_pants.rb
+++ Lesson1/Pifagor_pants.rb
@@ -19,49 +19,37 @@ p "Need only 3 parameters for this(3 sides). "
 p "So tell me FIRST side of triangle in santimetres"
 
 #takes user's input and save to 'side_a' variable
-side_a = gets.chomp.to_i
+side_a = gets.chomp.to_f
 
 p "Tell me the SECOND parameter - second side"
 
 #takes user's input and save to 'side_b' variable
-side_b = gets.chomp.to_i
+side_b = gets.chomp.to_f
 
 p "Fine. Tell me the THIRD parameter - third side"
 #takes user's input and save to 'side_c' variable
-side_c = gets.chomp.to_i
+side_c = gets.chomp.to_f
 p "Ok, wait a little, please"
 
 # main logic to check triangle. Find hypotenuse(longest side) 
-if (side_a>side_b)&&(side_a>side_b)
-	cathetus1 = side_b
-	cathetus2 = side_c
-	hypotenuse = side_a
-	elsif (side_b>side_a)&&(side_b>side_c)
-		cathetus1 = side_a
-		cathetus2 = side_c
-		hypotenuse = side_b
-	else
-		cathetus1 = side_a
-		cathetus2 = side_b
-		hypotenuse = side_c
-		
-	end
+sides = [side_a, side_b, side_c]
+hypotenuse  = sides.max
+sides.delete(hypotenuse)
+cathetus1 = sides[0]
+cathetus2 = sides[1]
+
 sleep(2)
 #check type of triangle
 # First case - check Piphagor's theory
 # Second  - try to find equal sides(but not all)
 # Third   - try to compare all sides. Find all eaqual case
 
-if (hypotenuse**2) == (cathetus1**2) + (cathetus2**2)
-	p "This is right triangle, Pifagor confirmed"
-
-elsif (side_a == side_b)&&(side_b != side_c)||(side_b == side_c)&&(side_a != side_c)||(side_a == side_c)&&(side_b != side_c)
-	p "This is isosceles triangle"
-elsif (side_a==side_b)&&(side_b==side_c)&&(side_a==side_c)
-	p "This is isosceles equilateral triangle. Not a right triangle, sorry("	
+if hypotenuse**2 == cathetus1**2 + cathetus2**2
+  message = "This is right triangle, Pifagor confirmed"
+  message += " And this is isosceles triangle" if cathetus1 == cathetus2
+  puts message
+elsif sides.uniq.length == 1
+  p "This is  equilateral triangle."	
 else
-	p "This is triangle with diff sides(all)."
+  p "Not a right triangle, sorry("
 end	
-
-
-

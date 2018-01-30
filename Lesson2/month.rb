d5a76ad772ac38066c331787e0f60963e6cab478
Stanislav Karpinsky <wildkain@gmail.com>
2018-01-30 11:17:43 +0300

    Fix ver1

--- Lesson2/month.rb
+++ Lesson2/month.rb
@@ -2,6 +2,19 @@
 Сделать хеш, содеращий месяцы и количество дней в месяце.
 В цикле выводить те месяцы, у которых количество дней ровно 30
 =end
-months = {janyary: 31, febrary: 28, march: 31, april: 30, may: 31, june: 30, july: 31, august: 31, september: 30, october: 31, november: 30, december: 31}
+months = {
+  janyary: 31,
+  febrary: 28,
+  march: 31,
+  april: 30,
+  may: 31,
+  june: 30,
+  july: 31,
+  august: 31,
+  september: 30,
+  october: 31,
+  november: 30,
+  december: 31
+  }
 
-months.each {|month, days| puts "#{month}: #{days}" if days.eql?(30)}
+months.each { |month, days| puts "#{month}: #{days}" if days.eql?(30) }

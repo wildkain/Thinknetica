493b9103c1f4f543748ebb7fc11c944af34d8cac
Stanislav <wildkain@gmail.com>
2018-02-08 13:55:21 +0300

    Refator Train.rb

--- Lesson3/main.rb
+++ Lesson3/main.rb
@@ -9,7 +9,7 @@ station2 = Station.new("Voronezh")
 station3 = Station.new("Povorino")
 moscow = Station.new("Moscow")
 piter = Station.new("Piter")
-london = Station.new("london")
+london = Station.new("London")
 
 route1 = Route.new(station, station2)
 route2 = Route.new(moscow, piter)
@@ -40,19 +40,36 @@ p "Cейчас  - - #{two.current_station.name}"
 p "Отправляем на следующую станцию"
 two.move_forward
 p "Прибыли, теперь мы в  - -#{two.current_station.name}"
-p "В лондоне теперь поездов: #{london.trains.count}"
+p "В #{two.current_station.name} теперь поездов: #{two.current_station.trains.count}"
 sleep(1)
-p "Отправляем на следующую станцию"
-two.move_forward
-p "Прибыли, теперь мы в  - -#{two.current_station.name}"
-p "В Лондоне теперь поездов: #{london.trains.count}"
-p "В Питере теперь поездов: #{piter.trains.count}"
-sleep(1)
-p "Забыл выключить утюг"
-p "Возвращаемся"
-two.move_backward
-p "Прибыли, теперь мы в  - -#{two.current_station.name}"
 
-p "В Лондоне теперь поездов: #{london.trains.count}"
-p "В Питере теперь поездов: #{piter.trains.count}"
-p "--------------------------------------"
+p two.current_station
+two.move_forward
+p two.current_station
+two.move_backward
+p two.current_station
+two.move_backward
+two.move_backward
+two.move_backward
+two.move_backward
+two.move_forward
+
+p "_______________________"
+
+p two.current_station
+#p two.route.stations.each {|st| p st.name}
+
+
+
+p two.current_speed
+#p two.wagons
+p two.speed_up(80)
+p two.speed_up(80)
+p two.speed_up(80)
+p two.speed_up(80)
+p two.speed_up(80)
+p two.current_speed
+#p two.change_speed(-40)
+#p two.current_speed
+#two.add_wagons(5)
+#p two.wagons
--- Lesson3/train.rb
+++ Lesson3/train.rb
@@ -6,56 +6,97 @@ class Train
     @number    = number
     @type      = type
     @wagons = wagons.to_i
+    @speed = 0
   end
 
-  def speed_up
-    @speed += 10
+  # не смог придумать, как реализовать изменение скорости в одном методе
+  # по-этому разделил на два
+  def speed_up(value = 1)
+    if value < 0 || current_speed >= 200
+      return
+    elsif @speed + value > 200
+      @speed += (@speed + value) - 200
+    else
+     @speed += value
+    end
   end
+
+  def speed_down(value = 1)
+    if @speed >= value
+      @speed -= value
+    else
+      @speed = 0
+    end
+  end
+
 
   def stop
     @speed = 0
   end
 
-  def say_route
-    p self.route.stations if self.route
+  def current_speed
+    @speed
   end
 
   def setup_route(route)
-    self.route = route
+    @route = route
     @station_index = 0
   end
 
+  #избавился от puts, проверка на маршрут
   def current_station
-    @current_station = self.route.stations[@station_index]
+   @current_station = @route.stations[@station_index] if @route
   end
 
   def last_station
-    p self.route.stations.last
+     @route.stations.last.name if @route
   end
 
   def next_station
-    p self.route.stations[@station_index + 1].name
+    @route.stations[@station_index + 1].name if @route
   end
 
+# добавил проверку на нахождение на конечной. изменил прибытие - убытие со станции.
   def move_forward
-    @station_index += 1
-    self.route.stations[@station_index - 1].train_out(self)
-    self.route.stations[@station_index].take_train(self)
+    return if last_station?
+    @current_station.train_out(self)
+    @station_index += 1 if @station_index
+    @route.stations[@station_index].take_train(self)
   end
 
+# добавил проверку на нахождение на начальной станции. изменил прибытие - убытие со станции.
   def move_backward
-    @station_index -= 1 if @station_index != 0
-    self.route.stations[@station_index + 1].train_out(self)
-    self.route.stations[@station_index].take_train(self)
+    return if first_station?
+    @current_station.train_out(self)
+    @station_index -= 1 if @station_index
+    @route.stations[@station_index].take_train(self)
   end
 
+# добавил проверку скорости
   def add_wagons(vagons = 1)
-    stop
-    @wagons += vagons
+    if @speed == 0
+      @wagons += vagons
+    else
+      p "Нельзя прицеплять вагоны на ходу. Сбросьте скорость"
+    end
+  end
+# добавил проверку скорости
+  def delete_wagons(vagons = 1)
+    if @speed == 0
+      @wagons -= vagons
+    else
+      p "Нельзя отцеплять вагоны на ходу. Сбросьте скорость"
+    end
   end
 
-  def delete_wagons(vagons = 1)
-    stop
-    @wagons -= vagons
+  private
+
+  def last_station?
+    current_station == @route.stations.last
   end
+
+  def first_station?
+    current_station == @route.stations.first
+  end
+
 end
